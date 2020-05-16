dfe760136cee4bc048e5750b14df243d342d6c3c
wildlyclassyprince <lihtumb@gmail.com>
2020-05-16 12:29:03 +0400

    Initial commit - test and write minimalist PlayerServer function

---
+++ server.go
@@ -0,0 +1,11 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// PlayerServer takes the request sent in
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "20")
+}
