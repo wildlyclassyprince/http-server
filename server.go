1616ca6437eca2a9850926f3c5643b769ec6ee14
wildlyclassyprince <lihtumb@gmail.com>
2020-05-16 13:04:15 +0400

    Test and add support for multiple player scores

--- server.go
+++ server.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-// PlayerServer takes the request sent in
+// PlayerServer implements the `Handler` by type casting
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }
