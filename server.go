cb2f5a21bad731daef346d13ef8f475f35f57f1d
wildlyclassyprince <lihtumb@gmail.com>
2020-05-16 13:11:40 +0400

    Add extra verbose function comments

--- server.go
+++ server.go
@@ -6,13 +6,15 @@ import (
 	"strings"
 )
 
-// PlayerServer implements the `Handler` by type casting
+// PlayerServer implements the `Handler` by type casting. It takes an HTTP request
+// to GET a player's score given the name, and POSTS (prints) the player's score.
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	fmt.Fprint(w, GetPlayerScore(player))
 }
 
+// GetPlayerScore takes the player name and returns the score
 func GetPlayerScore(name string) string {
 	if name == "Pepper" {
 		return "20"
