040eb5227cfc9b3c6e79ed649f28f3e63e0af0cb
wildlyclassyprince <lihtumb@gmail.com>
2020-05-18 19:22:09 +0400

    Extend 'StubPlayerStore' with 'RecordWin' method

--- server_test.go
+++ server_test.go
@@ -8,12 +8,17 @@ import (
 )
 
 type StubPlayerStore struct {
-	scores map[string]int
+	scores   map[string]int
+	winCalls []string
 }
 
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
 	score := s.scores[name]
 	return score
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
 }
 
 func TestGetPlayers(t *testing.T) {
