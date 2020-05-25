2f9078186a678c224502e5d66dbd3cb35fc4b5bd
wildlyclassyprince <lihtumb@gmail.com>
2020-05-25 15:26:24 +0400

    Moved 'InMemoryPlayerStore' implements to 'InMemoryPlayerStore.go'

--- main.go
+++ main.go
@@ -5,26 +5,6 @@ import (
 	"net/http"
 )
 
-// NewInMemoryPlayerStore initializes store
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
-
-// InMemoryPlayerStore implements GetPlayerScore
-type InMemoryPlayerStore struct {
-	store map[string]int
-}
-
-// GetPlayerScore returns the player's score given the name
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
-// RecordWin stores the win
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
 func main() {
 	server := &PlayerServer{NewInMemoryPlayerStore()}
 
