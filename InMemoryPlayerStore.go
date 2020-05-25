d08942c2669b15e90b564739df5d5921d8af7537
wildlyclassyprince <lihtumb@gmail.com>
2020-05-25 20:13:54 +0400

    Update 'GetLeague()' to return populated object

--- InMemoryPlayerStore.go
+++ InMemoryPlayerStore.go
@@ -22,5 +22,9 @@ func (i *InMemoryPlayerStore) RecordWin(name string) {
 
 // GetLeague returns player names and scores in JSON
 func (i *InMemoryPlayerStore) GetLeague() []Player {
-	return nil
+	var league []Player
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
 }
