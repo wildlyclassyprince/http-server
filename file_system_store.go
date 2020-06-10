a456533a89e3dde18994f1a830613b38ff5a3786
wildlyclassyprince <lihtumb@gmail.com>
2020-06-11 00:16:47 +0400

    Implement 'GetPlayerScore'

--- file_system_store.go
+++ file_system_store.go
@@ -15,3 +15,17 @@ func (f *FileSystemPlayerStore) GetLeague() []Player {
 	league, _ := NewLeague(f.database)
 	return league
 }
+
+// GetPlayerScore returns the player's score
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
+}
