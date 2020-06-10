bd00378f8fd639f00c48b208c378128813de38ff
wildlyclassyprince <lihtumb@gmail.com>
2020-06-11 00:11:35 +0400

    Add 'ReadSeeker' to io interface

--- file_system_store.go
+++ file_system_store.go
@@ -6,11 +6,12 @@ import (
 
 // FileSystemPlayerStore stores database objects
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 // GetLeague stores player scores in JSON file
 func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
 }
