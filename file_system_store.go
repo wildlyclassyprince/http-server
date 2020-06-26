89eb04c4c9bd8f9b24421f950ecfa72a8358cca3
wildlyclassyprince <lihtumb@gmail.com>
2020-06-27 01:59:02 +0400

    Update 'RecordWin' by removing 'Seek()'

--- file_system_store.go
+++ file_system_store.go
@@ -7,7 +7,7 @@ import (
 
 // FileSystemPlayerStore stores database objects
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
@@ -16,7 +16,7 @@ func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStor
 	database.Seek(0, 0)
 	league, _ := NewLeague(database)
 	return &FileSystemPlayerStore{
-		database: database,
+		database: &tape{database},
 		league:   league,
 	}
 }
@@ -48,6 +48,5 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	f.database.Seek(0, 0)
 	json.NewEncoder(f.database).Encode(f.league)
 }
