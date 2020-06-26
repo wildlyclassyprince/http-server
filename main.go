f52cadc13829c24ad8cda66de8a2528c7f149c50
wildlyclassyprince <lihtumb@gmail.com>
2020-06-27 01:38:25 +0400

    Persist player scores in file between restarts

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	store := NewInMemoryPlayerStore()
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
 	server := NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
